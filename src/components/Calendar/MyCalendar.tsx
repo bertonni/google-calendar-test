43aca096bda4cca1692757379354310be13a87dc
bertonnipaz <bertonnipaz@gmail.com>
2022-10-06 23:42:50 -0300

    getting google calendar events on fullcalendar component

--- src/components/Calendar/MyCalendar.tsx
+++ src/components/Calendar/MyCalendar.tsx
@@ -2,20 +2,27 @@ import "@fullcalendar/react/dist/vdom";
 import FullCalendar from "@fullcalendar/react";
 import dayGridPlugin from "@fullcalendar/daygrid";
 import timeGridPlugin from "@fullcalendar/timegrid";
-import { useState } from "react";
+import googleCalendarPlugin from '@fullcalendar/google-calendar';
 import ptBrLocale from "@fullcalendar/core/locales/pt-br";
 
 const MyCalendar = () => {
-  const [view, setView] = useState<string>("dayGridMonth");
 
   return (
     <div className="w-full">
       <FullCalendar
         selectable={true}
-        headerToolbar={{ left: 'dayGridMonth,timeGridWeek,timeGridDay', center: 'title', right: 'today,prevYear,prev,next,nextYear' }}
-        plugins={[dayGridPlugin, timeGridPlugin]}
+        headerToolbar={{
+          left: "dayGridMonth,timeGridWeek,timeGridDay",
+          center: "title",
+          right: "today,prevYear,prev,next,nextYear",
+        }}
+        plugins={[dayGridPlugin, timeGridPlugin, googleCalendarPlugin]}
         height={600}
-        initialView={view}
+        googleCalendarApiKey={import.meta.env.VITE_CALENDAR_API_KEY}
+        events={{
+          googleCalendarId:
+            import.meta.env.VITE_CALENDAR_ID,
+        }}
         locale={ptBrLocale}
       />
     </div>
